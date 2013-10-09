2e9f2896a1278873bfd8a6c2cc997e699b4fb1c0
Charley Hutchison <glenwayguy@gmail.com>
2013-10-08 22:27:16 -0500

    Set up provision.sh properly.

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
  # Install Node.js
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get -y update
