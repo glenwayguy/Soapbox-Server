3dfc5e9af0be6298e251a8f548e12c390f52286c
Charley Hutchison <glenwayguy@gmail.com>
2013-10-08 20:56:00 -0500

    Initial Commit. Vagrantfile for Vagrant VM and provision.sh for provisioning vm.

---
+++ provision.sh
@@ -0,0 +1,12 @@
+ # Install Node.js
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get -y update
+sudo apt-get -y install nodejs
+
+apt-get -y upgrade
+
+sudo npm install -g express
+
+
